15458b1f4aed93115f32b37d3dcf262eb30790c8
oushujun <shujun.ou.1@gmail.com>
2017-06-08 16:58:52 -0400

    Clean more outputs

--- bin/cleanOutput.pl
+++ bin/cleanOutput.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $index='';
-$index=$ARGV[0];
+my ($index, $LINE, $DNA, $PlantP)=('', '', '', '');
+($index, $LINE, $DNA, $PlantP)=@ARGV[0,1,2,3];
 die "ERROR: Usage: perl cleanOut.pl index\n" if $index eq '';
+
+`rm dummy060817.fa* $LINE.* $DNA.* $PlantP.* alluniRefprexp082813 Tpases020812DNA Tpases020812LINE`;
 
 `rm $index.cat.gz $index.LTRlib.clust.clstr $index.LTRlib $index.LTRlib.fa.n* $index.ltrTE* $index.nmtf $index.prelib* $index.retriever.scn*`;
 
