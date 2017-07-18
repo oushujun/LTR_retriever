95da61391dfb6fe628062068551253afcc8c7277
Shujun Ou <shujun.ou.1@gmail.com>
2017-07-18 11:54:17 -0400

    change perl script access permissions

--- bin/cleanOutput.pl
+++ bin/cleanOutput.pl
@@ -5,7 +5,7 @@ my ($index, $LINE, $DNA, $PlantP)=('', '', '', '');
 ($index, $LINE, $DNA, $PlantP)=@ARGV[0,1,2,3];
 die "ERROR: Usage: perl cleanOut.pl index\n" if $index eq '';
 
-`rm $LINE.* $DNA.* $PlantP.* alluniRefprexp082813 Tpases020812DNA Tpases020812LINE 2>/dev/null`;
+`rm $LINE.* $DNA.* $PlantP.* alluniRefprexp082813* Tpases020812DNA* Tpases020812LINE* 2>/dev/null`;
 
 `rm $index.cat.gz $index.LTRlib.clust.clstr $index.LTRlib $index.LTRlib.fa.n* $index.ltrTE* $index.nmtf $index.prelib* $index.retriever.scn.* 2>/dev/null`;
 
