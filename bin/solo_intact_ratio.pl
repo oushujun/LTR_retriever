4e42d61e093363bb9102cfa78a8eed3a161be12d
Shujun Ou <shujun.ou.1@gmail.com>
2019-04-16 18:12:45 -0500

    make the code work with solo_finder.pl and intact_finder_coarse.pl outputs

--- bin/solo_intact_ratio.pl
+++ bin/solo_intact_ratio.pl
@@ -4,38 +4,49 @@ use strict;
 #compute solo-intact LTR ratio for given lists
 #Shujun Ou (oushujun@msu.edu)
 #07-25-2017
+#update: 04/16/2019
 
-my $usage="perl this_script.pl solo_count intact_count > solo_intact_ratio";
+my $usage="\n\tperl solo_intact_ratio.pl solo_list intact_list > solo_intact_ratio\n
+	To make solo and intact lists:
+		perl solo_finder.pl RepeatMasker.out > solo_list
+		perl intact_finder_coarse.pl RepeatMasker.out > intact_list\n\n";
 
-open Solo, "<$ARGV[0]" or die $!;
-open Intact, "<$ARGV[1]" or die $!;
+open Solo, "<$ARGV[0]" or die $usage;
+open Intact, "<$ARGV[1]" or die $usage;
 
 my %all;
 
 while (<Intact>){
 	chomp;
 	s/^\s+//;
-	my ($id, $intact)=(split);
-	$all{$id}=["0", $intact];
+	my $id=(split)[0];
+	if (exists $all{$id}){
+		$all{$id}[1]++; #count intact number
+		} else {
+		$all{$id}=["0", "0"]; #initialize count of solo and intact number
+		}
 	}
 close Intact;
 
 while (<Solo>){
 	chomp;
 	s/^\s+//;
-	my ($id, $solo)=(split);
+	my $id=(split)[0];
 	if (exists $all{$id}){
-		$all{$id}[0]=$solo;
+		$all{$id}[0]++; #count solo number
 		} else {
-		$all{$id}=[$solo, "0"];
+		$all{$id}=["0", "0"];
 		}
 	}
+close Solo;
+
+print "LTR_fam\tSolo_count\tIntact_count\tSolo-Intact-ratio\n";
 foreach my $id (sort {$a cmp $b} keys %all){
 	my $ratio="NA";
 	my ($solo, $intact)=($all{$id}[0], $all{$id}[1]);
 	if ($intact==0){
-		$ratio="inf";
-		} 
+		$ratio="inf"; #no intact LTR found for this family
+		}
 	elsif ($solo==0){
 		$ratio=0;
 		}
@@ -44,5 +55,4 @@ foreach my $id (sort {$a cmp $b} keys %all){
 		}
 	print "$id\t$solo\t$intact\t$ratio\n";
 	}
-close Solo;
 
