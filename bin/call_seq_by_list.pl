f0e1964a25e0fc0988c84851cce9138f2aa88ead
Shujun Ou <shujun.ou.1@gmail.com>
2023-01-30 23:25:28 -0500

    update this script

--- bin/call_seq_by_list.pl
+++ bin/call_seq_by_list.pl
@@ -7,6 +7,8 @@ eg1. perl call_seq_by_LOC.pl array_list itself >result	##call LOC sequence withi
 eg2. perl call_seq_by_LOC.pl array_list -C your_database up_2000 >result	##call sequence of upper 2000 bp region in the list, from the provided database
 
 Update history:
+	v2.5	do not output sequence with all Ns (2023/01/30)
+	v2.4	output sequences without headers
 	v2.3	output a list of entirely excluded sequences
 	v2.2	support -ex. Exclude the sequence information provided by list and output the rest. Can use with -cov
 	v2.1	support - direction. If the locus position is upside down, eg. Chr1:2..1, it would be treated as negative strand request	Shujun Ou 2014/8/19
@@ -26,8 +28,9 @@ Update history:
 
 my $position='';
 my $range='itself'; ##defalut
+my $header=1; #1, output sequence headers (default); 0, no headers
 my $length=0; ### default get the LOC seq itself
-my $rmvoid=0; #0 for output empty sequences anyways; 1 for output only non-empty sequences
+my $rmvoid=0; #0 for output empty sequences anyways; 1 for output only non-empty sequences. Sequenece with all Ns or Xs are also considered empty.
 my $exclude=0; #0 for output sequence specified by list (default); 1 for exclude sequence specified by list
 my $coverage=1; #work with $exclude, if the excluded portion is too long (default 1, [0-1]), discard the entire sequence
 my $purge=0; #work with $exclude, switch on=1/off=0(default) to clean up aligned region and joint unaligned sequences
@@ -50,6 +53,7 @@ foreach my $para (@ARGV){
 		} ##default range
 	$genome=$ARGV[$i+1] if $para=~/^-C$/i;
 	$rmvoid=1 if $para=~/^-rmvoid$/i;
+	$header=$ARGV[$i+1] if $para=~/^-header$/i;
 	$coverage=$ARGV[$i+1] if $para=~/^-cov$/i;
 	$purge=$ARGV[$i+1] if $para=~/^-purge$/i;
 	$exclude=1 if $para=~/^-ex$/i;
@@ -86,6 +90,7 @@ my $chr_pre=$1 if (split /\s+/, $list[0])[1]=~/(.*):[0-9]+\.\.[0-9]+$/;
 my $str=1; #the coordinate of the first bp of a sequence
 my $stp=length $genome{$chr_pre};
 my $seq='';
+my $base_num = 0; # count non-N bases in $seq
 
 foreach my $line (@list){
 	chomp $line;
@@ -133,7 +138,11 @@ foreach my $line (@list){
 			$seq=reverse $seq; ### get a reverse sequence
 			($start, $stop)=($stop, $start);
 			}
-		print ">$chr:$start..$stop|$loc\n$seq\n" unless ($seq=~/^\s+$/ and $rmvoid==1); ###print out target sequence
+		$base_num = $seq =~ tr/[acgtACGT]/[acgtACGT]/;
+		unless ($base_num == 0 and $rmvoid==1){ ###print out target sequence
+			print ">$chr:$start..$stop|$loc\n" if $header == 1;
+			print "$seq\n";
+			}
 		}
 	my $cov;
 	if ($exclude==1){
@@ -143,9 +152,11 @@ foreach my $line (@list){
 			if (($stp-length $seq)/$stp >= $coverage){
 				print Exclude "$chr_pre\n";
 				} elsif ($purge==1){
-				print ">$chr_pre\n$seq\n";
+				print ">$chr_pre\n" if $header==1;
+				print "$seq\n";
 				} else {
-				print ">$chr_pre\n$genome{$chr_pre}\n";
+				print ">$chr_pre\n" if $header==1;
+				print "$genome{$chr_pre}\n";
 				}
 
 #			if ($purge==1){
@@ -178,9 +189,11 @@ foreach my $line (@list){
 		if (($stp-length $seq)/$stp >= $coverage){
 			print Exclude "$chr_pre\n";
 			} elsif ($purge==1){
-			print ">$chr_pre\n$seq\n";
+			print ">$chr_pre\n" if $header==1;
+			print "$seq\n";
 			} else {
-			print ">$chr_pre\n$genome{$chr_pre}\n";
+			print ">$chr_pre\n" if $header==1;
+			print "$genome{$chr_pre}\n";
 			}
 #		if ($purge==1){
 #			print ">$chr\n$seq\n" unless ($stp-length $seq)/$stp >= $coverage;
@@ -189,7 +202,10 @@ foreach my $line (@list){
 #			print ">$chr\n$genome{$chr}\n" unless ($stp-length $seq)/$stp >= $coverage;
 #			}
 		foreach my $chr (keys %genome){
-			print ">$chr\n$genome{$chr}\n" unless exists $chr{$chr};
+			unless (exists $chr{$chr}) {
+				print ">$chr\n" if $header==1;
+				print "$genome{$chr}\n";
+				}
 			}
 		}
 
