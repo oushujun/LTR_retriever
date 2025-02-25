2b748f7e55b1ad42cd49042e68963a59841e8215
oushujun <shujun.ou.1@gmail.com>
2025-02-25 11:02:50 -0500

    improve wrapping and reporting

--- bin/solo_intact_ratio_wrap.sh
+++ bin/solo_intact_ratio_wrap.sh
@@ -17,18 +17,25 @@ echo "Library: $lib"
 echo "RepeatMasker out: $RM_out\n"
 
 echo "1. Analyze library..."
-perl "$script_dir/find_LTR.pl" -lib "$lib" > "$lib.info"
+if [ ! -s "$lib.info" ]; then
+    perl "$script_dir/find_LTR.pl" -lib "$lib" > "$lib.info"
+else
+    echo "Skipping analysis, $lib.info already exists and is not empty."
+fi
 
-echo "2. Find solo LTRs..."
-perl "$script_dir/solo_finder.pl" -i "$RM_out" -info "$lib.info" > "$RM_out.solo"
+echo "2. Extract LTR entries..."
+grep LTR $RM_out > $RM_out.LTR
 
-echo "3. Find intact LTRs..."
-perl "$script_dir/intact_finder_coarse.pl" "$RM_out" > "$RM_out.intact"
+echo "3. Find solo LTRs..."
+perl "$script_dir/solo_finder.pl" -i "$RM_out.LTR" -info "$lib.info" > "$RM_out.solo"
 
-echo "4. Calculate solo:intact ratios..."
-perl "$script_dir/solo_intact_ratio.pl" "$RM_out.solo" "$RM_out.intact" > "$RM_out.SI.ratio"
+echo "4. Find intact LTRs..."
+perl "$script_dir/intact_finder_coarse_v2.pl" "$lib.info" "$RM_out.LTR" > "$RM_out.intact"
 
-echo "5. Overall solo:intact ratio:"
+echo "5. Calculate solo:intact ratios..."
+grep -v TE_ID "$RM_out.intact" | perl "$script_dir/solo_intact_ratio.pl" "$RM_out.solo" - > "$RM_out.SI.ratio"
+
+echo "6. Overall solo:intact ratio:"
 sh "$script_dir/total_solo_intact_calc.sh" "$RM_out.SI.ratio"
 
-echo "\nDone!"
+echo "Done!"
--- bin/total_solo_intact_calc.sh
+++ bin/total_solo_intact_calc.sh
@@ -4,7 +4,7 @@
 input_file=$1
 
 # Use awk to sum columns 2 and 3, then calculate the ratio
-awk '
+awk -v input_file="$input_file" '
 NR > 1 {  # Skip the header row
     sum2 += $2
     sum3 += $3
@@ -15,7 +15,7 @@ END {
     } else {
         ratio = sum2 / sum3
     }
-    print "Total_solo\tTotal_intact\tOverall_SI_ratio"
-    printf "%d\t%d\t%s\n", sum2, sum3, ratio
+    print "Genome\tTotal_solo\tTotal_intact\tOverall_SI_ratio"
+    printf "%s\t%d\t%d\t%s\n", input_file, sum2, sum3, ratio
 }
 ' "$input_file"
